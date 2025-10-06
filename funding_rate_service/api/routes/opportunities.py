248eec38669896b83beed538b5478fd35e466ad6
simbayippy <simbayippy@gmail.com>
2025-10-07 01:21:54 +0800

    update defauly filter for min divergence to 0.05% and min vol 1mil

--- funding_rate_service/api/routes/opportunities.py
+++ funding_rate_service/api/routes/opportunities.py
@@ -30,11 +30,11 @@ async def get_opportunities(
     exclude_dexes: Optional[str] = Query(None, description="Comma-separated list of DEXs to exclude"),
     
     # Profitability filters
-    min_divergence: Optional[Decimal] = Query(Decimal('0.0001'), description="Minimum divergence"),
+    min_divergence: Optional[Decimal] = Query(Decimal('0.0005'), description="Minimum divergence, default is 0.05%"),
     min_profit: Optional[Decimal] = Query(Decimal('0'), description="Minimum net profit percent"),
     
     # Volume filters
-    min_volume: Optional[Decimal] = Query(Decimal('100000'), description="Minimum 24h volume in USD"),
+    min_volume: Optional[Decimal] = Query(Decimal('1000000'), description="Minimum 24h volume in USD"),
     max_volume: Optional[Decimal] = Query(None, description="Maximum 24h volume in USD"),
     
     # OI filters (for low OI farming!)
