5e08943ce5f322ad765738c46b9372e59263ccdf
simbayippy <simbayippy@gmail.com>
2025-10-14 16:47:48 +0800

    remove dashboard reporter from operations init

--- strategies/implementations/funding_arbitrage/operations/__init__.py
+++ strategies/implementations/funding_arbitrage/operations/__init__.py
@@ -3,11 +3,9 @@
 from .position_opener import PositionOpener
 from .opportunity_scanner import OpportunityScanner
 from .position_closer import PositionCloser
-from .dashboard_reporter import DashboardReporter
 
 __all__ = [
     "PositionOpener",
     "OpportunityScanner",
     "PositionCloser",
-    "DashboardReporter",
 ]
