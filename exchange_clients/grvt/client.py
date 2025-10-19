f1060b81a1f0e2e9a6dc9e6b4655837770b2c7cd
simbayippy <simbayippy@gmail.com>
2025-10-20 01:06:30 +0800

    add for grvt

--- exchange_clients/grvt/client.py
+++ exchange_clients/grvt/client.py
@@ -17,6 +17,7 @@ from exchange_clients.base_models import (
     OrderInfo,
     query_retry,
     MissingCredentialsError,
+    ExchangePositionSnapshot,
     validate_credentials,
 )
 from helpers.unified_logger import get_exchange_logger
@@ -443,6 +444,13 @@ class GrvtClient(BaseExchangeClient):
         self.logger.log("[GRVT] get_account_balance not yet implemented", "DEBUG")
         return None
     
+    async def get_position_snapshot(self, symbol: str) -> Optional[ExchangePositionSnapshot]:
+        """
+        Get position snapshot for a symbol using official SDK.
+        """
+        # TODO
+        return None
+
     async def get_leverage_info(self, symbol: str) -> Dict[str, Any]:
         """
         Get leverage information for GRVT.
